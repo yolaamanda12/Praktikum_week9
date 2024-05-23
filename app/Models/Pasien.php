0d02bb43b5a5185bd741af417ffaabdd40d8243e
yolaamanda12 <147458765+yolaamanda12@users.noreply.github.com>
2024-05-23 09:08:37 -0700

    Add files via upload

---
+++ app/Models/Pasien.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pasien extends Model
+{
+    use HasFactory;
+    protected $table = 'pasien';
+// table adalah property laravel yang didalam nya terkoneksi dengan
+// nama table yang mau kita tampilkan data nya,
+// tetapi hanya dapat di akses di dalam class model tersebut ataupun turunan nya
+
+protected $fillable = ['id','kode','nama','tmp_lahir','tgl_lahir','gender'
+,'email','alamat','kelurahan_id'];
+// fillable adalah variable dalam model eloquent yang tugasnya
+// membatasi kolom mana yang dapat di isi atau di input
+}
